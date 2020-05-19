70d171285ff356a46ff15735722a7f908a645cfc
Glitch (steady-fragrant-comb) <none>
2020-05-19 15:26:33 +0000

    JSX

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import Babel from "@babel/core";
+
+const todos = ["Go to market", "Buy food", "Make dinner"];
+const lists = [];
+for (var todo of todos) {
+  let list = React.createElement("li", null, todo);
+  lists.push(list);
+}
+const App = React.createElement("ul", null, ...lists);
+const root = document.getElementById("root");
+ReactDOM.render(App, root);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
